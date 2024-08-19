32f49deab1bacc83db01db8ea0266fa6a0a831bb
Ron <a158.ron@gmail.com>
2024-08-19 17:04:14 +1000

    fixed issue by adding the clean function

--- src/Timer.js
+++ src/Timer.js
@@ -1,9 +1,15 @@
 import { useEffect } from "react";
 
 export default function Timer({ handleDecreaseTimeFF, secondsRemaining }) {
-  useEffect(function () {
-    setInterval(handleDecreaseTimeFF, 1000);
-  }, []);
+  useEffect(
+    function () {
+      const id = setInterval(handleDecreaseTimeFF, 1000);
+      return function () {
+        clearInterval(id);
+      };
+    },
+    [handleDecreaseTimeFF]
+  );
 
   const minutesLeft = (secondsRemaining / 60) | 0;
   const secondsLeft = secondsRemaining % 60;
